f9694cf5f8f1fb0917c8ac1d94dd078b501689d8
Viviane Mayra <vivi.mayra01@gmail.com>
2023-11-25 00:21:07 -0300

    Criação da contagem do carrinho

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,12 +1,17 @@
 import { Handbag } from "@phosphor-icons/react"
 import { CartButtonContainer } from "./styles"
 import { ComponentProps } from "react"
+import { useCart } from "../../hooks/useCart"
 
 type CartButtonProps = ComponentProps<typeof CartButtonContainer>
 
 export function CartButton({ ...rest }: CartButtonProps) {
+  const { cartItems } = useCart()
+
+  const cartQuantity = cartItems.length
   return (
     <CartButtonContainer {...rest}>
+      {cartQuantity >= 1 && <span>{cartQuantity}</span>}
       <Handbag weight="bold" size={24} />
     </CartButtonContainer>
   )
--- src/components/CartButton/styles.ts
+++ src/components/CartButton/styles.ts
@@ -2,11 +2,11 @@ import { styled } from "../../styles"
 
 export const CartButtonContainer = styled("button", {
   display: "flex",
+  position: "relative",
   alignItems: "center",
   justifyContent: "center",
   border: "none",
   borderRadius: 6,
-  position: "relative",
 
   "&:disabled": {
     opacity: 0.6,
@@ -19,6 +19,22 @@ export const CartButtonContainer = styled("button", {
   width: "3rem",
   height: "3rem",
   marginRight: "100px",
+
+  span: {
+    position: "absolute",
+    width: "1.25rem",
+    height: "1.25rem",
+    borderRadius: "50%",
+    top: "-6px",
+    right: "-6px",
+    color: "white",
+    background: "$green",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "0.75rem",
+    fontWeight: 700,
+  },
 
   "@media (max-width: 480px)": {
     marginRight: 0,
@@ -34,6 +50,9 @@ export const CartButtonContainer = styled("button", {
         color: "$white",
         background: "$green",
         marginRight: "0px",
+        span: {
+          visibility: "hidden",
+        },
       },
     },
   },
